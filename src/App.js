11df3e24a56e5a0f3f2e896f67c42cebd6042b82
장동윤 <bigstake82@naver.com>
2023-12-12 11:15:55 +0900

    라우터까지

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
+import { useState } from 'react';
 import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom'
+import './App.css';
+import data from './data.js';
 
-function App() {
+function App(){
+  let [shoes, setShoes]=useState(data)
+
   return (
     <div>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg' />
+      <div className="container">
+        <div className="row">
+          {shoes.map((a,i) => {
+            return (
+              <Card shoes={shoes[i]} />)
+            })
+          }
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">상품명</h4>
+            <p>상품설명</p>
+            <p>120000원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
+
+    <Routes>
+      <Route path="/detail" element={ <div>상세페이지임</div> } />
+      <Route path="/" element={ <div>메인페이지에서 보여줄거</div> } /> 
+    </Routes>
+    
+    <Link to="/">홈 </Link>
+    <Link to="/detail">상세페이지</Link>
+
+      
     </div>
-  );
+
+  )
 }
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={props.shoes.img} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+    </div>
+    )
+}
+
 
 export default App;
