85a582f54a0716d4c9e23bc0c450b003b8536800
장동윤 <bigstake82@naver.com>
2024-01-01 22:34:49 +0900

    240101 2234

--- src/App.js
+++ src/App.js
@@ -2,34 +2,52 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import $ from 'jquery';
 import './App.css';
+
 import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import styled from 'styled-components'
+
 import { Routes, Route, Link } from 'react-router-dom'
-import './App.css';
-import data from './data.js';
+import data from './data';
+
+
+let Box = styled.div`
+  padding : 20px;
+  color : grey
+`;
+let YellowBtn = styled.button`
+  background : yellow;
+  color : black;
+  padding : 10px;
+`;
 
 function App(){
   let [shoes, setShoes]=useState(data)
 
   return (
     <div>
+      <YellowBtn>안녕</YellowBtn>
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+
       <div className='main-bg' />
       <div className="container">
         <div className="row">
           {shoes.map((a,i) => {
-            return (
-              <Card shoes={shoes[i]} />)
-            })
-          }
+            return(
+            <div className="col-md-4">
+            <img src={shoes[i].img} width="80%" />
+            <h4>{shoes[i].title}</h4>
+            <p>{shoes[i].content}</p>
+          </div>)
+          })}
         </div>
       </div>
 
